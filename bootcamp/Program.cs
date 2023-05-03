5358bed230e137171a770134406944a1299cd2c2
A9219 <alexey1978malyshev@gmail.com>
2023-05-04 00:11:22 +0300

    multipleTable added

---
+++ bootcamp/Program.cs
@@ -0,0 +1,24 @@
+﻿
+int n = Convert.ToInt32(Console.ReadLine());
+
+int[,] multTable = new int[n,n];
+int a = 1, b = 1;
+for (int i = 0; i < multTable.GetLength(0); i++)
+{
+    for (int j = 0; j < multTable.GetLength(1); j++)
+    {
+        multTable[i,j] = a * b;
+        b++;
+    }
+    b = 1;
+    a++;    
+}
+
+for (int i = 0; i < multTable.GetLength(0); i++)
+{
+    for (int j = 0; j < multTable.GetLength(1); j++)
+    {
+        System.Console.Write(multTable[i,j] + " * ");
+    }
+    System.Console.WriteLine();
+}
